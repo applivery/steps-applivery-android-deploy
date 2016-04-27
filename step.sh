611595d1081c1f3a69c8c30c068d4e9ecd231dff
César Trigo <cesartrigo@gmail.com>
2016-04-27 23:12:46 +0200

    Update step.sh

--- step.sh
+++ step.sh
@@ -22,7 +22,7 @@ if [ ! -f "${apk_path}" ] ; then
 fi
 
 # APPLIVERY API TOKEN
-if [ -z "${APPLIVERY_API_TOKEN}" ] ; then
+if [ -z "${api_token}" ] ; then
   echo "# Error"
   echo '* No App APPLIVERY_API_TOKEN provided as environment variable. Terminating...'
   echoStatusFailed
@@ -30,7 +30,7 @@ if [ -z "${APPLIVERY_API_TOKEN}" ] ; then
 fi
 
 # APPLIVER APP ID
-if [ -z "${APPLIVERY_APP_ID}" ] ; then
+if [ -z "${app_id}" ] ; then
   echo "# Error"
   echo '* No App APPLIVERY_APP_ID provided as environment variable. Terminating...'
   echoStatusFailed
@@ -39,8 +39,17 @@ fi
 
 ############# DEFINITIONS ##############
 
-api_token="${APPLIVERY_API_TOKEN}"
-app_id="${APPLIVERY_APP_ID}"
+bitrise_build_number="${BITRISE_BUILD_NUMBER}"
+git_repository_url="${GIT_REPOSITORY_URL}"
+bitrise_app_url="${BITRISE_APP_URL}"
+bitrise_build_url="${BITRISE_BUILD_URL}"
+bitrise_build_trigger_timestamp="${BITRISE_BUILD_TRIGGER_TIMESTAMP}"
+bitrise_git_branch="${BITRISE_GIT_BRANCH}"
+bitrise_git_tag="${BITRISE_GIT_TAG}"
+bitrise_git_commit="${BITRISE_GIT_COMMIT}"
+bitrise_git_message="${BITRISE_GIT_MESSAGE}"
+bitrise_provision_url="${BITRISE_PROVISION_URL}"
+bitrise_certificate_url="${BITRISE_CERTIFICATE_URL}"
 
 echo
 echo "========== CONFIGS =========="
@@ -50,8 +59,21 @@ echo "* version_name: ${version_name}"
 echo "* notes: ${notes}"
 echo "* notify: ${notify}"
 echo "* os: ${os}"
+echo "* deployer: ${deployer}"
 echo "* tags: ${tags}"
 echo "* apk_path: ${apk_path}"
+echo "***** Other variables *****"
+echo "* bitrise_build_number: ${bitrise_build_number}"
+echo "* git_repository_url: ${git_repository_url}"
+echo "* bitrise_app_url: ${bitrise_app_url}"
+echo "* bitrise_build_url: ${bitrise_build_url}"
+echo "* bitrise_build_trigger_timestamp: ${bitrise_build_trigger_timestamp}"
+echo "* bitrise_git_branch: ${bitrise_git_branch}"
+echo "* bitrise_git_tag: ${bitrise_git_tag}"
+echo "* bitrise_git_commit: ${bitrise_git_commit}"
+echo "* bitrise_git_message: ${bitrise_git_message}"
+echo "* bitrise_provision_url: ${bitrise_provision_url}"
+echo "* bitrise_certificate_url: ${bitrise_certificate_url}"
 echo
 
 ###########################
@@ -63,8 +85,20 @@ curl_cmd="$curl_cmd -F \"versionName=${version_name}\""
 curl_cmd="$curl_cmd -F \"notes=${notes}\""
 curl_cmd="$curl_cmd -F \"notify=${notify}\""
 curl_cmd="$curl_cmd -F \"os=android\""
+curl_cmd="$curl_cmd -F \"deployer=bitrise\""
 curl_cmd="$curl_cmd -F \"tags=${tags}\""
 curl_cmd="$curl_cmd -F \"package=@${apk_path}\""
+curl_cmd="$curl_cmd -F \"bitrise_build_number=${bitrise_build_number}\""
+curl_cmd="$curl_cmd -F \"git_repository_url=${git_repository_url}\""
+curl_cmd="$curl_cmd -F \"bitrise_app_url=${bitrise_app_url}\""
+curl_cmd="$curl_cmd -F \"bitrise_build_url=${bitrise_build_url}\""
+curl_cmd="$curl_cmd -F \"bitrise_build_trigger_timestamp=${bitrise_build_trigger_timestamp}\""
+curl_cmd="$curl_cmd -F \"bitrise_git_branch=${bitrise_git_branch}\""
+curl_cmd="$curl_cmd -F \"bitrise_git_tag=${bitrise_git_tag}\""
+curl_cmd="$curl_cmd -F \"bitrise_git_commit=${bitrise_git_commit}\""
+curl_cmd="$curl_cmd -F \"bitrise_git_message=${bitrise_git_message}\""
+curl_cmd="$curl_cmd -F \"bitrise_provision_url=${bitrise_provision_url}\""
+curl_cmd="$curl_cmd -F \"bitrise_certificate_url=${bitrise_certificate_url}\""
 curl_cmd="$curl_cmd https://dashboard.applivery.com/api/builds"
 
 echo
